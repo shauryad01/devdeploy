8d925bdaac2d8a69d397f0605c0372165ebc02cc
Shaurya Dhingra <shauryadhingra1@gmail.com>
2026-05-23 16:43:10 +0530

    Correct issues in main

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-func main(){
+func main() {
 	fmt.Println("DevDeploy Starting...")
 
-	r:=gin.Default()
-	r.GET("/ping",func(c *gin.Context){
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message":"pong",
+			"message": "pong",
 		})
-	}
+	})
 	r.Run(":8000")
 }
-
