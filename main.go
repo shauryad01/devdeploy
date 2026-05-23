9ecaabe8ab797bd1d96b8354f6cb0ec21fe6fdb9
Shaurya Dhingra <shauryadhingra1@gmail.com>
2026-05-23 08:42:07 +0000

    Add go module and main

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"net/http"
 	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main(){
 	fmt.Println("DevDeploy Starting...")
+
+	r:=gin.Default()
+	r.GET("/ping",func(c *gin.Context){
+		c.JSON(http.StatusOK, gin.H{
+			"message":"pong",
+		})
+	}
+	r.Run(":8000")
 }
 
