f944e5dede0590f720f8995f2f1f9e1ed1a9df24
Shaurya Dhingra <shauryadhingra1@gmail.com>
2026-06-06 19:03:42 +0530

    core deployment pipeline setup (worker + docker)

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"strconv"
 	"time"
 
@@ -36,8 +37,8 @@ func main() {
 	}
 	log.Println("database connected:", conn.Stats())
 
-	jobs := make(chan int, 100)
-	go worker(jobs, conn)
+	jobs := make(chan int, 100) //job buffered queue
+	go worker(jobs, conn)       //goroutine for worker
 
 	r := gin.Default()
 
@@ -75,7 +76,7 @@ func main() {
 			})
 			return
 		}
-		jobs <- d.ID
+		jobs <- d.ID //push id into jobs buffered queue
 		c.JSON(http.StatusCreated, d)
 	})
 
@@ -107,6 +108,63 @@ func main() {
 
 func worker(jobs chan int, db *sqlx.DB) {
 	for id := range jobs {
-		fmt.Printf("processing %d", id)
+		var d Deployment
+		query := "SELECT * from deployments WHERE id = $1"
+		err := db.Get(&d, query, id)
+		targetDir := fmt.Sprintf("./repos/%d", id)
+
+		if err == sql.ErrNoRows {
+			log.Printf("error: repo not found for id: %d", id)
+			continue
+
+		}
+		if err != nil {
+			log.Printf("error: Failed to fetch repo for id: %d", id)
+			continue
+
+		}
+		//CLONING GIT REPO
+		updateQ := "UPDATE deployments SET status = $1 where id = $2"
+		db.Exec(updateQ, "cloning", id)
+		cmd := exec.Command("git", "clone", d.RepoURL, targetDir)
+		err = cmd.Run()
+		if err != nil {
+			db.Exec(updateQ, "failed", id)
+			log.Println("clone failed:", err)
+			continue
+		}
+		db.Exec(updateQ, "cloned", id)
+		log.Println("cloned deployment:", id)
+
+		//BUILDING DOCKER IMAGE
+		db.Exec(updateQ, "building", id)
+		imageName := fmt.Sprintf("devdeploy-%d", id)
+		dockerbld := exec.Command("docker", "build", "-t", imageName, targetDir)
+		dockerbld.Stdout = os.Stdout
+		dockerbld.Stderr = os.Stderr
+		err = dockerbld.Run()
+		if err != nil {
+			db.Exec(updateQ, "failed", id)
+			log.Println("docker build failed:", err)
+			continue
+		}
+
+		//STARTING DOCKER CONTAINER
+		db.Exec(updateQ, "starting", id)
+		port := fmt.Sprintf("%d", 4000+id)
+		containerName := fmt.Sprintf("devdeploy-%d", id)
+		run := exec.Command("docker", "run", "-d", "-p", port+":3000", "--name", containerName, imageName)
+		run.Stdout = os.Stdout
+		run.Stderr = os.Stderr
+		err = run.Run()
+		if err != nil {
+			db.Exec(updateQ, "failed", id)
+			log.Println("docker run failed:", err)
+			continue
+		}
+		db.Exec(updateQ, "running", id)
+		log.Printf("Deployment %d running on port %s", id, port)
 	}
+
+	//generate nginx config file
 }
