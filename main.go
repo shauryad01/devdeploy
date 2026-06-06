c7c664647acd9c8e5a229e56e072b67b7cc4dd3a
Shaurya Dhingra <shauryadhingra1@gmail.com>
2026-06-06 16:55:07 +0530

    Simplify modules

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/shauryad01/devdeploy/db"
 )
+
+type CreateDeploymentRequest struct {
+	RepoURL string `json:"repo_url"`
+}
+
+type Deployment struct {
+	ID        int       `db:"id"`
+	RepoURL   string    `db:"repo_url"`
+	Status    string    `db:"status"`
+	CreatedAt time.Time `db:"created_at"`
+}
 
 func main() {
 	godotenv.Load()
@@ -20,12 +34,69 @@ func main() {
 		log.Fatal("database connection failed: ", err)
 	}
 	log.Println("database connected:", conn.Stats())
+
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
+	r.POST("/deployments", func(c *gin.Context) {
+		var req CreateDeploymentRequest
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "invalid request",
+			})
+			return
+		}
+
+		if req.RepoURL == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "repo_url is required",
+			})
+			return
+		}
+
+		var d Deployment
+
+		query := "INSERT INTO deployments (repo_url) VALUES ($1) RETURNING *"
+		row := conn.QueryRowx(query, req.RepoURL)
+		err := row.StructScan(&d)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "failed to create Deployment",
+			})
+			return
+		}
+
+		c.JSON(http.StatusCreated, d)
+	})
+
+	r.GET("/deployments/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"Error": "Invalid or empty ID"})
+			return
+		}
+
+		var d Deployment
+
+		query := "SELECT * FROM deployments WHERE id = $1"
+		err = conn.Get(&d, query, id)
+
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "deployment not found"})
+			return
+		}
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch deployment"})
+			return
+		}
+		c.JSON(http.StatusOK, d)
+
+	})
 	r.Run(":8000")
 }
